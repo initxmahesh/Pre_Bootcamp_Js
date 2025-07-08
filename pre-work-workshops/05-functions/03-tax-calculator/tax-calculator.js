56fd8ac738a36c52a41ee2b5c5760ffdb9acd1e6
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-08 13:58:15 +0545

    11th task

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function taxCalculator(price, state) {
+    if (state === 'NY')
+        return 0.04 * price + price;
+    else if (state === 'NJ')
+        return 0.06625 * price + price;
+}
+
+console.log(taxCalculator(100, 'NY'))
