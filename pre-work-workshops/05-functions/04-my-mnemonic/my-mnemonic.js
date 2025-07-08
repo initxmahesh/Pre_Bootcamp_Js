27f517ef89df72b4b9506749a6abf671c07e936b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-08 14:55:16 +0545

    12th task

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function myMnemonic(str1, str2, str3, str4) {
+    if (str1 && str2 && str3 && str4) {
+        return str1[0] + str2[0] + str3[0] + str4[0];
+    }
+    else if (str1 && str2 && str3)
+        return str1[0] + str2[0] + str3[0];
+    else if (str1 && str2)
+        return str1[0] + str2[0];
+    else
+        return 'string';
+}
+
+console.log(myMnemonic("We", "Eat"));
+console.log(typeof(myMnemonic("We", "Eat")));
