373fa396bd64f51392f63f533aee24fea557ea9e
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-08 13:51:17 +0545

    10th task

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function defaultGreet(firstName, lastName) {
+    if (firstName && lastName) {
+        return 'Hi ' + firstName + ' ' + lastName + '!';
+    }
+    else (firstName)
+    return 'Hi ' + firstName + ' ' + 'Doe!';
+}
+console.log(defaultGreet('initx','mahesh'));
