e8b2d0094ba0f39867cf0635accc7b229eb44708
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-08 13:34:52 +0545

    8th task

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,13 @@
-let name = 'David';
+// let name = 'David';
 // let name = 'Nimit';
-// let name = 'Someone else';
+let name = 'Someone else';
 
 let found;
 
 // YOUR CODE BELOW
+if (name == 'David' || name == 'Nimit') {
+    found = true;
+}
+else if (name == 'Someone else') found = false;
+
+console.log(found);
