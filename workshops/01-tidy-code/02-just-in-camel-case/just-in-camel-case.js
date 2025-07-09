266c8cd2abaa2dca45390b2fa39c223df97ff090
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 11:50:55 +0545

    19th task

--- workshops/01-tidy-code/02-just-in-camel-case/just-in-camel-case.js
+++ workshops/01-tidy-code/02-just-in-camel-case/just-in-camel-case.js
@@ -1,18 +1,18 @@
 // YOUR CODE BELOW
-function justInCamelCase(undername) {
-  let camelcaseoutput = "";
-    let foundunder = false;
-    for(let i = 0; i<undername.length; i++) {
-    if (undername[i] === "_") {
-      foundunder = true;
-      } else {
-        if (foundunder) {
-        camelcaseoutput += undername[i].toUpperCase();
-      foundunder = false;
+function justInCamelCase(underName) {
+  let camelCaseOutput = "";
+  let foundUnder = false;
+  for (let i = 0; i < underName.length; i++) {
+    if (underName[i] === "_") {
+      foundUnder = true;
     } else {
-      camelcaseoutput += undername[i];
+      if (foundUnder) {
+        camelCaseOutput += underName[i].toUpperCase();
+        foundUnder = false;
+      } else {
+        camelCaseOutput += underName[i];
       }
+    }
   }
-  }
-      return camelcaseoutput;
+  return camelCaseOutput;
 }
