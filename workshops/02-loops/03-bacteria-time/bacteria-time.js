136a07a68af52818f90bb63dbc46124f7e7737f2
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 14:03:18 +0545

    23rd task

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function bacteriaTime(currentNum, targetNum) {
+    debugger;
+  let time = 0;
+  if (targetNum < currentNum) return "targetNum must be larger than currentNum";
+  else {
+    for (let i = currentNum; i < targetNum; i *= 2) {
+      time += 20;
+    }
+  }
+  return time;
+}
+
+bacteriaTime(1,8)
