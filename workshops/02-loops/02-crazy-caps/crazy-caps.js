23cdc411fc1ce18a52bed824b185d73c50d187dd
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 13:32:19 +0545

    22th task

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+function crazyCaps(str) {
+  debugger;
+  let result = "";
+  for (i = 0; i < str.length; i++) {
+    if (i % 2 === 0) {
+      result += str[i].toLowerCase();
+    } else result += str[i].toUpperCase();
+  }
+  return result;
+}
+crazyCaps("Mahesh is a Tej Fellow");
