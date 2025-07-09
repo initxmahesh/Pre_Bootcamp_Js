0959ad25f72f47b95cc12b6643c76c107fff6105
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 17:56:27 +0545

    Truthy done

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function isTruthy(param) {
+  if (param === "")
+    return "The empty string is falsey (the only falsey string)";
+  else if (param === null) return "The null value is falsey";
+  else if (param === false) return "The boolean value false is falsey";
+  else if (param === undefined) return "undefined is falsey";
+  else if (param === 0)
+    return "The number 0 is falsey (the only falsey number)";
+  else return true;
+}
