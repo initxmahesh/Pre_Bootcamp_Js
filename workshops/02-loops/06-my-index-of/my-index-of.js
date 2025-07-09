ad18ffa0ae8a0a6d9590f20030bd4392411239eb
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 00:41:31 +0545

    index of problem done

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function myIndexOf(source, searchValue, startIdx = 0) {
+  for (let i = startIdx; i < source.length; i++) {
+    for (let j = 0; j < searchValue.length; j++) {
+      if (source[i + j] == searchValue[j]) {
+        if (j == searchValue.length - 1) return i;
+      } else break;
+    }
+  }
+  return -1;
+}
