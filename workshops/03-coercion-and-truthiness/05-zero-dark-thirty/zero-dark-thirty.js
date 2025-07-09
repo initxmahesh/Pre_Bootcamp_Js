59618ed54cad6c3676480f7466f59326a1155c61
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 19:48:45 +0545

    zero-dark-thirty done

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,29 @@
+// Zero Dark Thirty
+
+// Write a function `zeroDarkThirty` that accepts a number as an arguemnt.
+
+// `zeroDarkThirty` should return a number with all of the zeroes removed:
+
+// ```javascript
+// zeroDarkThirty(102302) // => 1232
+// zeroDarkThirty(606.203) // => 66.23
+// ```
+
+// If the number 0 is passed in as the argument, return [NaN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN).
+
+// ```javascript
+// zeroDarkThirty(0) // => NaN
+// ```
+
+
 // YOUR CODE BELOW
+function zeroDarkThirty(arg) {
+  if (arg === 0) return NaN;
+
+  let str = String(arg);
+  let result = "";
+  for (i = 0; i < str.length; i++) {
+    if (str[i] !== "0") result += str[i];
+  }
+  return Number(result);
+}
