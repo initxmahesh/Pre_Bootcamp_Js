b331c739f7a03fa72e946c7ec1e75553af241ed8
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 00:43:49 +0545

    most vowel done

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+
+function mostVowels(strg) {
+  const vowel = "aeiouAEIOU";
+  let strgArr = strg.split(" ");
+  let max = 0;
+  let mostVwl = "";
+  for (let i = 0; i < strgArr.length; i++) {
+    let word = strgArr[i].replace(/[^a-zA-Z]/g, '');
+    let count = 0;
+    for (let j = 0; j <= word.length; j++) {
+      let letter = word[j];
+      if (vowel.includes(letter)) {
+        count++;
+      }
+    }
+    if (count > max) {
+      max = count;
+      mostVwl = word;
+    }
+  }
+  return max === 0 ? "" : mostVwl;
+}
