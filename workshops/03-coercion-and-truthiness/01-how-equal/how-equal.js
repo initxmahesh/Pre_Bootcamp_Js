8d94ea12784b9adf9d3d48a08a3a5bb024e8fe39
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 17:42:10 +0545

    equal js done

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,21 @@
+// How Equal
+
+// Define a function, `howEqual`, that accepts two arguments of any type.
+
+// `howEqual` should return the string 'loosely' if the two values passed into
+// the function are loosely equal. `howEqual` should return 'strictly' if the two
+// values are strictly equal. `howEqual` should return the string 'not equal' if
+// the two values are neither strictly nor loosely equal.
+
+// ```javascript
+// howEqual(0, '0'); // => loosely
+// howEqual(3, 9/3); // => strictly
+// howEqual(true, 'truthy') // => not equal
+// ```
+
 // YOUR CODE BELOW
+function howEqual(para1, para2) {
+  if (para1 === para2) return "strictly";
+  else if (para1 == para2) return "loosely";
+  else return "not equal";
+}
