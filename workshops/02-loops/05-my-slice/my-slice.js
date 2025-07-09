a7e660c77f533b02d5be7eec9600d98ed8b6a0c4
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 16:56:49 +0545

    25th task

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function mySlice(originalString, startIdx = 0, endIdx = originalString.length) {
+  let copy = "";
+  for (i = startIdx; i < endIdx; i++) {
+    copy += originalString[i];
+  }
+    return copy;
+}
+console.log(mySlice("Mahesh",2,5));
