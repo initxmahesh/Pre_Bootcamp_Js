929a11d9d3ec7e74145071e93a5754cc3277cea7
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 18:28:21 +0545

    my-or-my-and.js

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function myOr(arg1, arg2, arg3) {
+  if (arg1) return arg1;
+  else if (arg2) return arg2;
+  else return arg3;
+}
+
+function myAnd(arg1, arg2, arg3) {
+  if (!arg1) return arg1;
+  else if (!arg2) return arg2;
+  else return arg3;
+}
+console.log(myOr(false, "Hello", ""));
+console.log(myAnd(null, 0, "c"));
