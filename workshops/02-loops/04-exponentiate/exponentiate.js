7f8240dc984801050235d673199f4d60b1aa3926
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-09 14:16:05 +0545

    24th task

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function exponentiate(base, power) {
+  result = base;
+  if (power === 0) return 1;
+  else {
+    for (i = power; i > 1; i--) {
+      result *= base;
+    }
+  }
+  return result;
+}
