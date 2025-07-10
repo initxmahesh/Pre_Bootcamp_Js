032c8ef2b4b710ae4a5434634e18b1568de9497d
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 11:26:04 +0545

    my includes done

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+function myIncludes(ary, searchValue) {
+    for (i = 0; i < ary.length; i++){
+        if (ary[i] === searchValue)
+            return true;
+    }
+    return false;
+}
