9c682ad9002bc31a51974dcfb9c70d9a2034c99c
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 11:16:15 +0545

    array odd couple done

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,31 @@
+// Odd Couple
+
+// Define a function, `oddCouple`, that accepts an array of numbers as an argument.
+
+// `oddCouple` should return a new array with the first two odd numbers from the
+// original array:
+
+// ```javascript
+// oddCouple([1, 2, 3, 4, 5]); // => [1, 3]
+// ```
+
+// If fewer than two odd numbers exist in the original array, return an empty array
+// or an array with the only odd number:
+
+// ```javascript
+// oddCouple([10, 15, 20]); // => [15]
+// oddCouple(2, 4, 6, 8); // => []
+// ```
+
 // YOUR CODE BELOW
+function oddCouple(ary) {
+  let newArray = [];
+  for (let i = 0; i < ary.length; i++) {
+    if (ary[i] % 2 !== 0) {
+      newArray.push(ary[i]);
+      if (newArray.length === 2) break;
+    }
+  }
+  return newArray;
+}
+console.log(oddCouple([2, 4]));
