803a0d03c599932da3ab8968d1e6506a45415579
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 20:49:55 +0545

    my last index done

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function myLastIndexOf(array, searchValue, startIdx=array.length) {
+    for (let i = startIdx - 1; i >= 0; i--) {
+        if (array[i] === searchValue)
+            return i
+    }
+    return -1
+}
+console.log(
+  myLastIndexOf(["the", "girls", "bring", "the", "boys", "out"], "the", 2)
+);
