4e4394bb7b8634984e282f2b7f1164ad7ad47c6c
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 17:12:58 +0545

    make grid done

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function makeGrid(numColumns, numRows) {
+    let newArray = [];
+    for (i = 0; i < numRows; i++) {
+        let tempArray = []
+        for (j = 0; j < numColumns; j++) {
+            tempArray.push(j+1)
+        }
+        newArray.push(tempArray);
+    }
+    return newArray;
+}
+
+
+console.log(makeGrid(3, 4));
