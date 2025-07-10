a0347e62ad754c0174e3237529b0b5939002d523
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 19:48:36 +0545

    slice done

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function mySlice(originalArray, startIdx = 0, endIdx = originalArray.length) {
+  let cpyArray = [];
+  if (startIdx < 0) {
+    startIdx = originalArray.length + startIdx;
+  }
+
+  if (endIdx < 0) {
+    endIdx = originalArray.length + endIdx;
+  }
+
+  for (let i = startIdx; i < endIdx; i++) {
+    let element = originalArray[i];
+    cpyArray.push(element);
+  }
+
+  return cpyArray;
+}
