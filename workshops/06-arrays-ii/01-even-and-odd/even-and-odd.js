7d30ff507aa80a1972ecc73cc5431a27b1358292
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 12:30:26 +0545

    even-and-odd done

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function evenAndOdd(array) {
+  let newArray = [[], []];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      if (array[i] % 2 === 0) {
+        newArray[0].push(array[i]);
+      } else {
+        newArray[1].push(array[i]);
+      }
+    }
+  }
+  return newArray;
+}
