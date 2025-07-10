e031a6d0445a07a9824c123ce1ddf20c834f3879
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-10 23:14:12 +0545

    compared done

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function compareObjects(obj1, obj2) {
+    debugger;
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) { 
+        return false
+    }
+    for (let [key, value] of Object.entries(obj1)) {
+        if (!(key in obj2)) return false
+        if (obj2[key] !== value) return false
+    }
+    return true
+}
+console.log(compareObjects({ name: 'zeke' }, { name: 'zeke'}))
