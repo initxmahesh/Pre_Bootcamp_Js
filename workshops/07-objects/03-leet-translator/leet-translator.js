9806f5a5e2647c4aacf906b17cdaae9c0c68d593
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 00:46:48 +0545

    leet-translator done

--- workshops/07-objects/03-leet-translator/leet-translator.js
+++ workshops/07-objects/03-leet-translator/leet-translator.js
@@ -2,14 +2,28 @@ let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
 
 // YOUR CODE BELOW
+// function leetTranslator(str) {
+//     let newString = ''
+//     for (let element of str.toLowerCase().split("")){
+//         if (letters.includes(element)) {
+//             let index = letters.indexOf(element);
+//             newString += leetChars[index]
+//         }
+//         else newString += element;
+//     }
+//     return newString
+// }
+
+
 function leetTranslator(str) {
-    let newString = ''
-    for (let element of str.toLowerCase().split("")){
-        if (letters.includes(element)) {
-            let index = letters.indexOf(element);
-            newString += leetChars[index]
-        }
-        else newString += element;
+    let newString = '';
+    let tempString = str.toLowerCase("")
+    let obj = {}
+    for (i = 0; i < letters.length; i++) {
+        obj[letters[i]]=leetChars[i]
+    }
+    for (j = 0; j < tempString.length; j++){
+        newString += obj[tempString[j]] 
     }
     return newString
 }
