82e618aee174fb43526aed578a8cd8d68f28ac03
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-12 00:35:19 +0545

    Chain reaction completed

--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+function chainReaction(startVal, array) {
+    let val = startVal
+    for (i = 0; i < array.length; i++){
+            val= array[i](val)
+    }
+    return val
+}
