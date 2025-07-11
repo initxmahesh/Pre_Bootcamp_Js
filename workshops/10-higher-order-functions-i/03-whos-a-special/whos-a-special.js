2b06c9eaa8f2784dbeb97b8cc9c5672932b582d6
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 21:48:54 +0545

    whos a special

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,35 @@
+// Who's a Special?
+// 
+// Write a function whosASpecial that takes an array of pets and returns a string
+// confirming what you already knew: all of the pets are very special!
+// Feel free to add your own special pets to the array!
+// 
+// Use .forEach in your answer.
+// 
+// 
+// ```javascript
+// let specialPets = [{
+//     name: 'Sadie',
+//     species: 'cat'
+//   }, {
+//     name: 'Layla',
+//     species: 'cat'
+//   }, {
+//     name: 'Bogie',
+//     species: 'dog'
+//   }
+// ];
+// 
+// whosASpecial(specialPets);
+// // => Sadie the cat is very special! Layla the cat is very special! Bogie the dog is very special!
+// ```
+
+
 // YOUR CODE BELOW
+let whosASpecial = function (arr) {
+    let newArray = '';
+    for (i = 0; i < arr.length; i++){
+        newArray += `${arr[i].name} the ${arr[i].species} is very special! `
+    }
+    return newArray.trim()
+}
