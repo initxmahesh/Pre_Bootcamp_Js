0982d1dfd134b4f57788d5d43e7dc5a0efa0cc07
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 20:26:29 +0545

    say my name done

--- workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,20 @@
+//  Say My Name
+// 
+// Define an object, `me`, that has a `name` property and a `getGreeting` method.
+// `getGreeting` should return a greeting.
+// 
+// ```javascript
+// console.log(me.name); // 'Kat'
+// me.getGreeting(); // => 'Hi, my name is Kat.'
+// ```
+
+
 // YOUR CODE BELOW
+let me = {
+    name: 'Kat',
+    getGreeting: function () {
+        return `Hi, my name is ${this.name}.`
+    }
+}
+// console.log(me.name); // 'Kat'
+// me.getGreeting(); // => 'Hi, my name is Kat.'
