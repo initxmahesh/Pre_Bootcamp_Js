8d7c3d6b60b5b2df151b9a75d40756a6da8dbad1
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 19:04:43 +0545

    finder function done

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,28 @@
+// ### Finder Function
+// 
+// Define a function 'finderFunction' that takes an array and a callback. Pass
+// each element from the array into the callback. If the callback returns true,
+// return the index of the current element. If the callback never returns true,
+// return -1;
+// 
+// ```javascript
+// let numbers = [1, 3, 5, 64, 7, 12];
+// let odds = [9, 13, 15, 17];
+// 
+// function isEven(num) {
+//   return !(num % 2);
+// }
+// 
+// console.log(finderFunction(numbers, isEven)) // 3
+// console.log(finderFunction(odds, isEven)) // -1
+// ```
+
 // YOUR CODE BELOW
+let finderFunction = function (array, callback) {
+    for (let i = 0; i < array.length; i++){
+        let element = array[i]
+        if (callback(element) === true)
+            return i;
+    }    
+    return -1
+}
