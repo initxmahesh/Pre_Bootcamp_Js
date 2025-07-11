8a83b840fae522f4a917dc662cff913e4de486de
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 13:19:09 +0545

    clone machine done

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function cloneMachine(parent) {
+    let child = { ...parent };
+    
+    child.name= parent.name + 'Clone';
+    child.offspring = [];
+    parent.offspring.push(child.name);
+
+    return child;
+}
