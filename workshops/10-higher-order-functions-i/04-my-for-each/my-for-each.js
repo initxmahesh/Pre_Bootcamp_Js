75f6e6f3ac0302672049e155d4c4a488123f65bc
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 22:10:49 +0545

    my for each done

--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,3 +1,23 @@
+// ### My For Each
+
+// Write a function myForEach that accepts an array and a callback function. The
+// behavior of myForEach should mirror the functionality of the native .forEach()
+// array method as closely as possible
+
+// ```javascript
+// let sum = 0;
+
+// function addToSum(num) {
+//     sum += num;
+// }
+
+// let nums = [1, 2, 3];
+
+// myForEach(nums, addToSum);
+
+// console.log(sum); // 6
+// ```
+
 // YOUR CODE BELOW
 function myForEach(arr, callback) {
     for (i = 0; i < arr.length; i++){
