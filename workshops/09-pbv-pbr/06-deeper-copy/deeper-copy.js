41c2afa535f5a45e65c605a4721f638c03c38ed0
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-11 18:00:11 +0545

    deeper copy done

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,31 @@
 // YOUR CODE BELOW
+function deeperCopy(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i]
+        if (Array.isArray(element)) {
+            let nestedElement = []
+            for (let j = 0; j < element.length; j++){
+                nestedElement.push(element[j])
+            }
+            newArray.push(nestedElement)
+        }
+        else newArray.push(element)
+
+
+    }            
+    return newArray
+    
+}
+
+let myArray = [1, [2, 3]];
+
+let copy = deeperCopy(myArray);
+copy[1].push(4);
+
+console.log(myArray); // [1, [2, 3]]
+
+let copy2 = myArray.slice();
+copy2[1].push(4);
+
+console.log(myArray); // [1, [2, 3, 4]]
