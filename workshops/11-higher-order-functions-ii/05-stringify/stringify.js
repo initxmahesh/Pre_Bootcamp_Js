e7ffbd3971edeba3318df1c99a03bb70bbc92813
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-12 20:54:18 +0545

    stringify done

--- workshops/11-higher-order-functions-ii/05-stringify/stringify.js
+++ workshops/11-higher-order-functions-ii/05-stringify/stringify.js
@@ -1,1 +1,25 @@
+// ### Stringify
+
+// Define a function, stringify, that takes a callback and returns a new function.
+// When the new function is called, it should call the callback, and return the
+// value returned by the callback, but not before explicitly coercing the returned
+// value to a string.
+
+
+// ```javascript
+// function returnsANumber() {
+//   return 100;
+// }
+
+// let newFunction = stringify(returnsANumber);
+// newFunction(); // => '100'
+// ```
+
+
 // YOUR CODE BELOW
+
+function stringify(callback) {
+    return function (){
+        return String(callback());
+    }
+}
