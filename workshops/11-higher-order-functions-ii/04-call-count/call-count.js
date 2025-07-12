6107e0c5d5dc9d268253aa53226973e68f901c61
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-12 13:32:53 +0545

    call count done

--- workshops/11-higher-order-functions-ii/04-call-count/call-count.js
+++ workshops/11-higher-order-functions-ii/04-call-count/call-count.js
@@ -1,1 +1,26 @@
+// ### Call Count
+
+// Write a function, callCount, that returns a new function. The new function
+// should return the number of times its been called.
+
+// ```javascript
+// let newFunction1 = callCount();
+// let newFunction2 = callCount();
+
+// newFunction1(); // => 1
+// newFunction1(); // => 2
+
+// newFunction2(); // => 1
+// newFunction2(); // => 2
+// ```
+
+
 // YOUR CODE BELOW
+
+function callCount() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
