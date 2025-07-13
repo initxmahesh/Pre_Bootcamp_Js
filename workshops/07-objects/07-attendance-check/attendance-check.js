866b39f589bf20ff8d7b59cc3572d0531e3f7304
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-13 08:49:48 +0545

    attedance checked :)

--- workshops/07-objects/07-attendance-check/attendance-check.js
+++ workshops/07-objects/07-attendance-check/attendance-check.js
@@ -1,40 +1,75 @@
+// ### Attendance Check
+
+// Define a function, `attendanceCheck`, that accepts a day of the week as a string.
+
+// Using the globally-defined classRoom array, `attendanceCheck` should return a
+// new array with only the names of the students present on the inputted day of
+// the week.
+
+// ```javascript
+
+// classCheck('Monday'); // => ['Marnie', 'Shoshanna']
+
+// classCheck('Wednesday'); // => ['Marnie', 'Lena']
+// ```
+
+
+
 let classRoom = [
-    {
-        "Marnie" : [
-                {"Monday" : true},
-                {"Tuesday" : true},
-                {"Wednesday" : true},
-                {"Thursday" : true},
-                {"Friday" : true}
-            ]
-    },
-    {
-        "Lena" : [
-                {"Monday" : false},
-                {"Tuesday" : false},
-                {"Wednesday" : true},
-                {"Thursday" : false},
-                {"Friday" : true}
-            ]
-    },
-    {
-        "Shoshanna" : [
-                {"Monday" : true},
-                {"Tuesday" : true},
-                {"Wednesday" : false},
-                {"Thursday" : true},
-                {"Friday" : false}
-            ]
-    },
-    {
-        "Jessa" : [
-                {"Monday" : false},
-                {"Tuesday" : false},
-                {"Wednesday" : false},
-                {"Thursday" : false},
-                {"Friday" : true}
-            ]
-    }
+  {
+    Marnie: [
+      { Monday: true },
+      { Tuesday: true },
+      { Wednesday: true },
+      { Thursday: true },
+      { Friday: true },
+    ],
+  },
+  {
+    Lena: [
+      { Monday: false },
+      { Tuesday: false },
+      { Wednesday: true },
+      { Thursday: false },
+      { Friday: true },
+    ],
+  },
+  {
+    Shoshanna: [
+      { Monday: true },
+      { Tuesday: true },
+      { Wednesday: false },
+      { Thursday: true },
+      { Friday: false },
+    ],
+  },
+  {
+    Jessa: [
+      { Monday: false },
+      { Tuesday: false },
+      { Wednesday: false },
+      { Thursday: false },
+      { Friday: true },
+    ],
+  },
 ];
 
 // YOUR CODE BELOW
+
+function attendanceCheck(str) {
+  let newArr = [];
+  for (let i = 0; i < classRoom.length; i++) {
+    let obj = classRoom[i];
+    let stud = Object.keys(obj)[0];
+    let attdarr = obj[stud];
+    for (let j = 0; j < attdarr.length; j++) {
+      let val2 = attdarr[j];
+      let dayName = Object.keys(val2)[0];
+      if (dayName === str && val2[dayName] === true) {
+        newArr.push(stud);
+        break;
+      }
+    }
+  }
+  return newArr;
+}
