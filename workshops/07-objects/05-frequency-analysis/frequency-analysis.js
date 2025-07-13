bdccb30a9d09b1f35f09161520bbc246af038810
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-13 08:12:54 +0545

    frequency-analysed done

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,29 @@
+// ### Frequency Analysis
+
+// Define a function `frequencyAnalysis` that accepts a string of lower-case
+// letters.
+
+// `frequencyAnalysis` should return an object containing the count for each letter
+// in the string.
+
+// ```javascript
+// frequencyAnalysis('abca'); // => {a: 2, b: 1, c: 1}
+// ```
+
+
 // YOUR CODE BELOW
+
+function frequencyAnalysis(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    let ele = str[i];
+    if (obj[ele] === undefined) {
+      let count = 0;
+      for (let j = 0; j < str.length; j++) {
+        if (str[i] === str[j]) count++;
+      }
+      obj[ele] = count;
+    }
+  }
+  return obj;
+}
